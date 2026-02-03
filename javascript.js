9bd939d8bbad17fdf61e300fb74ab7b94a7b93e1
CondurachiCristian <107665724+CondurachiCristian@users.noreply.github.com>
2026-02-03 11:37:37 +0200

    Add button that makes new grid and fits previous grid

--- javascript.js
+++ javascript.js
@@ -1,22 +1,47 @@
 const container = document.querySelector("#container");
 
-function makeDivs (times) {
-    for (let i = 0; i < times; i++) {
+function makeDivColumn (num) {
+    for (let j = 0; j < num; j++) {
+        makeDivRow(num);
+    }
+};
+
+function makeDivRow (num) {
+    for (let i = 0; i < num; i++) {
+        const total = 100;
+        let divisor = num;
+        let times = Math.floor(total / divisor);
         const div = document.createElement("div");
-        div.id = `div${i+1}`;
+        div.setAttribute("style", `flex : 1 0 ${times}%; padding-bottom : ${times}%;`);
         div.className = "squares";
         container.appendChild(div);
     }
 };
 
-makeDivs(256);
+makeDivColumn(16);
+
+function makeNewGrid () {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+    let input = prompt("Enter a number for the new Grid");
+    if (input > 100 || input < 0) {
+        alert("Please select a number lower than 100 and higher than 0");
+    } else if (!isNaN(input)) {
+        makeDivColumn(input);
+        const divs = document.querySelectorAll("#container > div");
+        divs.forEach((item) => item.addEventListener("mouseover", onHoverItem));  } else {
+        alert("Please enter a number");
+    }
+};
 
 function onHoverItem () {
-    console.log(this.id);
-    document.querySelector(`#${this.id}`).classList.remove("squares");
-    document.querySelector(`#${this.id}`).classList.add("newSquares");
+    this.classList.remove("squares");
+    this.classList.add("newSquares");
 };
 
 const divs = document.querySelectorAll("#container > div");
-divs.forEach((item) => item.addEventListener("mouseover", onHoverItem)
-);
+divs.forEach((item) => item.addEventListener("mouseover", onHoverItem));
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click" , makeNewGrid);
